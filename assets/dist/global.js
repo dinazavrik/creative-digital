e74040ad324efcef2f1a24f031fceb6a7689eaec
Unknown <avedenko.a@mail.ru>
2017-10-07 21:18:33 +0300

    Creative Full

---
+++ assets/dist/global.js
@@ -0,0 +1,36 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        $('a[href*="#"]').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+                location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1500);
+                }
+            }
+        });
+
+        $('.txt-slide-img__slider').slick({
+            dots: true,
+            infinite: true,
+        });
+
+        $('.txt-slider-icons__slider').slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1
+        });
+
+    });
+
+})(jQuery);
+
+
+
